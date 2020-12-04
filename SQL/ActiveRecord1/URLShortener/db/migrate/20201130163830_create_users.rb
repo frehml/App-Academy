786d70f6b28aa2f4320989b835ff92c23650dd39
frehml <frederic.hamelink@hotmail.com>
2020-12-04 17:31:12 +0100

    URL shortener retry

--- SQL/ActiveRecord1/URLShortener/db/migrate/20201130163830_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-    end
-    add_column :users, :email , :string
-    add_index :users, :email, unique: true
-  end
-end
--- SQL/ActiveRecord1/URLShortener/db/migrate/20201130190848_createshortened_urls.rb
+++
@@ -1,4 +0,0 @@
-class CreateshortenedUrls < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
